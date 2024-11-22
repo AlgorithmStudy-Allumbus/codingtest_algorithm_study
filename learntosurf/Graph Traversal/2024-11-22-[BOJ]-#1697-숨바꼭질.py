b45ab5895ea75deb2a3bc251fd82605ef4a6246c
learntosurf <jeoung1030@gmail.com>
2024-11-22 10:00:41 +0900

    [BOJ] #1697.숨바꼭질 / 실버1 / 60(∆)

---
+++ learntosurf/Graph Traversal/2024-11-22-[BOJ]-#1697-숨바꼭질.py
@@ -0,0 +1,24 @@
+from collections import deque
+import sys 
+input = sys.stdin.readline
+
+def bfs(start, K):
+    queue = deque()
+    queue.append(start)
+    visited[start] = 0
+    
+    while queue:
+        node = queue.popleft()
+        if node == K: # 동생의 위치에 도달했을 때 
+            print(visited[node])
+            return
+        
+        # 동생의 위치에 도달할 때까지, 3가지 이동 경우를 탐색 
+        for next in (node-1, node+1, node*2):
+           if (0 <= next < 100001) and not visited[next]:
+               visited[next] = visited[node] + 1
+               queue.append(next)
+
+N, K = map(int, input().split())
+visited = [False] * 100001
+bfs(N, K)
