ca752294e696736a8d7b9dcf65860f1e0448c0d8
learntosurf <jeoung1030@gmail.com>
2024-11-22 10:03:13 +0900

    [BOJ] #1697.숨바꼭질 / 실버1 / 60(∆)

--- learntosurf/Graph Traversal/2024-11-22-[BOJ]-#1697-숨바꼭질.py
+++ learntosurf/Graph Traversal/2024-11-22-[BOJ]-#1697-숨바꼭질.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 def bfs(start, K):
     queue = deque()
     queue.append(start)
-    visited[start] = 0
+    visited[start] = 0 # 시작 위치까지의 시간은 0으로 초기화
     
     while queue:
         node = queue.popleft()
@@ -16,9 +16,9 @@ def bfs(start, K):
         # 동생의 위치에 도달할 때까지, 3가지 이동 경우를 탐색 
         for next in (node-1, node+1, node*2):
            if (0 <= next < 100001) and not visited[next]:
-               visited[next] = visited[node] + 1
+               visited[next] = visited[node] + 1 # 이동시간을 1초 증가시킨다. 
                queue.append(next)
 
 N, K = map(int, input().split())
-visited = [False] * 100001
+visited = [-1] * 100001
 bfs(N, K)
