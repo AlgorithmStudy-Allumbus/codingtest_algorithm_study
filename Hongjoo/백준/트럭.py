0d02a2d1dae9271f4b5f1d93eab6264d6f0fa7da
Hongjoo <zaqquum01@gmail.com>
2024-12-09 21:53:29 +0900

    [BOJ]#13335트럭/ Gold/ 실패
    https://www.acmicpc.net/problem/13335

--- Hongjoo/백준/트럭.py
+++ Hongjoo/백준/트럭.py
@@ -1,11 +1,14 @@
-'''
-Condition 
-1. 다리 - 최대 하중 L , 최대 개수 W , 길이 W 
--단위 시간 에 거리
-
-'''
+# 실패
 import sys
-# 1. 입력 - 버스 개수 , 다리 길이 , 다리 최대 하중
-N , W , L = map(int, sys.stdin.readline.splite()) 
-buses = map(int, sys.stdin.readline.splite())
-#2. 매 time 마다 bridge 최대 하중을 넘지 않은채 올리기 
+n, w, l = map(int, sys.stdin.readline().split())
+truck = list(map(int, sys.stdin.readline().split()))
+time, bridge = 0, [0]*w #1. 시간변수와 다리의 길이를 입력받은 횟수만큼 리스트에 저장
+while bridge: #2. 다리의 길이가 0이 아닐때까지 반복
+    time += 1 #3 time +1
+    bridge.pop(0) #4.다리의 첫번째 원소 제거
+    if truck: #5.bridge리스트를 추가하는건 truck리스트가 남아있을때까지
+        if sum(bridge) + truck[0] <= l: #6기존 다리의 원소의 합 + 가장 왼쪽의 트럭'이 다리의 최대하중과 같거나 작다면.
+            bridge.append(truck.pop(0))
+        else: #7. 다리에 트럭이 있지만 다리에 올리지 못할 경우 다리 공간 유지하기 위해 0을 삽입
+            bridge.append(0) 
+print(time)
