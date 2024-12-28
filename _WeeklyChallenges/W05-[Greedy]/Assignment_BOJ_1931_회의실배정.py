aee3a545dea81ac381e150ce135dcc625fb1af69
learntosurf <jeoung1030@gmail.com>
2024-12-29 01:17:24 +1100

    [BOJ] #1931.회의실 배정 / 골드5 / 30(∆)

---
+++ _WeeklyChallenges/W05-[Greedy]/Assignment_BOJ_1931_회의실배정.py
@@ -0,0 +1,25 @@
+'''
+BOJ #1931. 회의실 배정 (골드5)
+https://www.acmicpc.net/problem/1931
+유형: Greedy
+출처: https://v3.leedo.me/devs/49
+'''
+
+n = int(input())
+
+meetings = []
+
+for _ in range(n):
+    start, end = map(int, input().split(" "))
+    meetings.append((start, end))
+# 종료 시간을 오름차순 정렬하고, 시작 시간을 오름차순 정렬합니다
+meetings.sort(key=lambda x: (x[1], x[0]))
+
+time = 0
+answer = 0
+for meeting in meetings:
+    if time <= meeting[0]:
+        time = meeting[1]
+        answer += 1
+
+print(answer)
