1a86117de2543244af64e3f54b73504f88a6d100
learntosurf <jeoung1030@gmail.com>
2025-01-11 23:44:44 +0900

    [BOJ] #2164.카드2 / 실버4 / 15분(∆)

--- learntosurf/Data Structures/2025-01-11-[BOJ]-#2164-카드2.py
+++ learntosurf/Data Structures/2025-01-11-[BOJ]-#2164-카드2.py
@@ -1,12 +1,12 @@
 import sys
-input = sys.stdin.read
+input = sys.stdin.readline
+from collections import deque
 
 N = int(input())
-L = list(range(1,N+1))
+cards = deque(range(1, N + 1))  # 1부터 N까지의 숫자를 덱에 저장
 
-while (len(L)>1):
-    L.pop(0)
-    temp = L.pop(0)
-    L.append(temp)
+while len(cards) > 1:
+    cards.popleft()  # 제일 위의 카드를 버림
+    cards.append(cards.popleft())  # 그다음 카드를 아래로 옮김
 
-print(L.pop(0))
+print(cards[0])  # 마지막으로 남은 카드 출력
