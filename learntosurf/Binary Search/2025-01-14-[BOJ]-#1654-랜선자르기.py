6252071af557ac395e2071ff698f8bd1f61ad528
learntosurf <jeoung1030@gmail.com>
2025-01-15 01:53:17 +0900

    [BOJ] #1654.랜선 자르기 / 실버2 / 20(O)

---
+++ learntosurf/Binary Search/2025-01-14-[BOJ]-#1654-랜선자르기.py
@@ -0,0 +1,19 @@
+import sys 
+input = sys.stdin.readline
+
+K, N = map(int, input().split())
+lan = [int(input()) for _ in range(K)]
+
+def binary_search(start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        lines = 0
+        for i in lan:
+            lines += i // mid
+        if lines >= N:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return end
+
+print(binary_search(1, max(lan)))
