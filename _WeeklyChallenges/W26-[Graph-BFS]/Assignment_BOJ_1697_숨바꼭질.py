e85eb218b940499481c5473cd863f5f8373b878d
Minjeong Kim <101111603+Mingguriguri@users.noreply.github.com>
2025-06-22 11:24:47 +0900

    Feat: 6월 2주차 과제 문제 정답 풀이 업로드

--- _WeeklyChallenges/W26-[Graph-BFS]/Assignment_BOJ_1697_숨바꼭질.py
+++ _WeeklyChallenges/W26-[Graph-BFS]/Assignment_BOJ_1697_숨바꼭질.py
@@ -4,4 +4,23 @@ https://www.acmicpc.net/problem/1697
 유형: Graph, BFS
 """
 
-# PR 올릴 때 정답도 함께 공개 예정
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+MAX = 100000
+N, K = map(int, input().split())
+
+visited = [0] * (MAX+1)
+queue = deque([N])
+
+while queue:
+    current = queue.popleft()
+    if current == K:
+        break
+    for nx in (current + 1, current - 1, current * 2):
+        if 0 <= nx <= MAX and not visited[nx]:
+            visited[nx] = visited[current] + 1
+            queue.append(nx)
+
+print(visited[K])
