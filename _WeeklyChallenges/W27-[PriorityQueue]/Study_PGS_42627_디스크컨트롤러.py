57ed95469d3d39f4bb0962bcd9cd881d28535e64
Minjeong Kim <101111603+Mingguriguri@users.noreply.github.com>
2025-06-30 22:54:04 +0900

    feat: 발제 문제 풀이2번 수정

--- _WeeklyChallenges/W27-[PriorityQueue]/Study_PGS_42627_디스크컨트롤러.py
+++ _WeeklyChallenges/W27-[PriorityQueue]/Study_PGS_42627_디스크컨트롤러.py
@@ -43,30 +43,27 @@ def solution(jobs):
 """
 import heapq
 
-def solution(jobs):
-   jobs.sort()     # 요청시간 기준 정렬
-   job_len = len(jobs)
-   i = 0           # jobs 인덱스
-   end_time = 0    # 현재 시간
-   return_time = 0 # 작업 반환 시간
-   count = 0       # 작업 처리한 개수
 
+def solution(jobs):
+   answer = 0
+   now = 0  # 현재시간
+   i = 0  # 처리개수
+   start = -1  # 마지막 완료시간
    heap = []
 
-   while count < job_len:
-       # 현재 시간에 요청된 작업 처리
-       while i < job_len and jobs[i][0] <= end_time:
-           heapq.heappush(heap, (jobs[i][1], jobs[i][0], i))  # 소요시간, 요청시간, 작업번호 순서
+   while i < len(jobs):
+       for job in jobs:
+           if start < job[0] <= now:
+               heapq.heappush(heap, [job[1], job[0]])
+
+       if heap:
+           current = heapq.heappop(heap)
+           start = now
+           now += current[0]
+           answer += now - current[1]  # 요청으로부터 처리시간
            i += 1
-
-       # 대기 큐에 작업이 있다면, 시간을 업데이트한다.
-       if len(heap) > 0:
-           work_time, start_time, num = heapq.heappop(heap)
-           end_time += work_time
-           return_time += end_time - start_time
-           count += 1
        else:
-           # 대기 큐가 비었다면, 다음 작업이 올 때까지 기다려야 한다.
-           end_time = jobs[i][0]
+           now += 1
 
-   return return_time // job_len
+   return answer // len(jobs)
+
